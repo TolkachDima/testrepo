5604cbb37ab16da46a5f5ec0f7d09ad72ffab0de
TolkachDima <tolkach.dima64@gmail.com>
2019-03-28 12:53:26 +0200

    install pug

--- ht18/site.js
+++ ht18/site.js
@@ -1,5 +1,10 @@
 var express = require("express");
+var app = express();
 
+app.get("/", function (req, res) {
+    res.send()
+});
 
+app.use(express.static(path.join(__dirname, "public")));
 
-
+app.listen(3000);
