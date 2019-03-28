2f34663c2a780550fc76758c9e62466d62a2abfe
TolkachDima <tolkach.dima64@gmail.com>
2019-03-28 14:57:29 +0200

    как сдлетаь так, чтобы при нажатии кнопки в alert показывался value input

---
+++ ht18/script.js
@@ -0,0 +1,8 @@
+var input = document.getElementById("name");
+var check = document.getElementById("FirstButt");
+    // if (check.onclick){
+    //     alert(";vjlsvjl'sdf")
+    // }
+    function send(){
+        alert("nameOfDiv.innerHtml");
+    }
