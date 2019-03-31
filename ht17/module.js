7442aa0e5b60601eed155282463c6e717041428e
TolkachDima <tolkach.dima64@gmail.com>
2019-03-31 17:53:04 +0300

    done callback task

--- ht17/module.js
+++ ht17/module.js
@@ -1,81 +1,24 @@
 var Converter = require("./converter");
 var request = require("request")
-var baseCurrencyUs = 27;
 
 
-// function f (callback) {
+
+function f (cb) {
+  request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR0__pQECzSU-WrckQqcQilKCJekyUNMjbtwVCXJZyllBSlZgE1M_RCKr8M', function get(error, response, body, sum) {
+    if (!error && response.statusCode == 200) {
   
-// request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR0__pQECzSU-WrckQqcQilKCJekyUNMjbtwVCXJZyllBSlZgE1M_RCKr8M', function (error, response, body) {
-// if (!error && response.statusCode == 200) {
-//     var baseCurrencyUs = JSON.parse(body);  
-//         var a = (data[2].buy);
-//         console.log(a);
-        
-//         var baseCurrencyUs = data[2].buy;
-//         return baseCurrencyUs;
-//         //callback(baseCurrencyUs);
-//   } else {
-//     console.warn(error);
-//     }
-//     callback(baseCurrencyUs);
-//   });
-  
-
-
-// // console.log(f());
-
-// var converter = new Converter(baseCurrencyUs);
-// console.log(converter.convertToUa(1000));
-// console.log(converter.convertToUs(1000));
-// }
-
-
-// console.log(f());
-
-
-//function f (callback) {
-  
-  request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR0__pQECzSU-WrckQqcQilKCJekyUNMjbtwVCXJZyllBSlZgE1M_RCKr8M', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
       var data = JSON.parse(body);  
-          var a = (data[2].buy);
-          console.log(a);
-          
-          // var baseCurrencyUs = null;  
-           //return data[2].buy;
-          
-          
+      var baseCurrencyUs = +(data[2].buy);
+      return cb(baseCurrencyUs);   
     } else {
       console.warn(error);
-      }
-      callback(baseCurrencyUs);
-      return a;
-    });
-    //return a;
-  
-  
-  //f();
- console.log(a);
-  var b = function () {
-    console.log(baseCurrencyUs);
+    }
+  });
   }
-   
+  f(function (baseCurrencyUs) {
   
   var converter = new Converter(baseCurrencyUs);
-  console.log(converter.convertToUa(1000));
-  console.log(converter.convertToUs(1000));
-  
-  
-  
-   console.log(f());
-  
-  
+console.log(converter.convertToUa(1000));
+console.log(converter.convertToUs(1000));
 
-
-
-
-
-
-
-
-
+});
--- ht17/test2.js
+++ ht17/test2.js
@@ -1,30 +1,21 @@
 var request = require('request');
 var sum = 29;
 
-function f (sum) {
+function f (cb) {
 request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR0__pQECzSU-WrckQqcQilKCJekyUNMjbtwVCXJZyllBSlZgE1M_RCKr8M', function get(error, response, body, sum) {
   if (!error && response.statusCode == 200) {
 
     var data = JSON.parse(body);  
     var a = +(data[2].buy);
-    console.log(a) 
-    return a;
-    sum = a;
-
-
-
-
-
-
-
-
-     
+    return cb(a);   
   } else {
     console.warn(error);
   }
 });
 }
-f();
+f(function(data){
+  console.log(data)
+});
 
 
 
